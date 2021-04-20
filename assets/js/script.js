caec0c55792b500a6b5ca9a58c36593da275a458
JAPinell <japinell@yahoo.com>
2021-04-20 16:36:48 -0500

    Added rendering of search city buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const API_ONECALL_END_POINT = "data/2.5/onecall";
 const API_KEY = "9dc0ecf3100ca1f98d7b3462ccb6b3df";
 const FORECAST_DAYS = 5; // 1 - 7
 const searchBtn = $("#searchButton");
-
+const lastSearchCitiesDiv = $("#last-searches");
 // Variables
 var today = dayjs();
 var todayText = dayjs().format("MM/DD/YYYY");
@@ -20,8 +20,14 @@ var cityWeather = {
   wind: "",
   humidity: "",
   uvIndex: "",
-  nextFiveDays: [],
+  fiveDaysForecast: [],
 };
+var lastSearch = {
+  cityName: "",
+  latitude: "",
+  longitude: "",
+};
+var lastSearchCities = []; // Stores an array of lastSearch objects
 
 //
 $("#todayDate").text(todayText);
@@ -43,21 +49,6 @@ function showMessage(message) {
   //
 }
 
-// Get search by city
-function getSearchByCity() {
-  //
-  var searchString = $("#searchText").val().trim();
-  //
-  if (searchString === "") {
-    //
-    searchString = "Overland Park,KS,US"; // Default
-    //
-  }
-  //
-  return searchString;
-  //
-}
-
 // Get weather icon
 function getWeatherIcon(icon) {
   //
@@ -67,7 +58,7 @@ function getWeatherIcon(icon) {
 }
 
 // Render weather data
-function renderWeatherData(data) {
+function renderWeatherData() {
   //
   var weatherDiv = $("#weather");
   var spanEl, innerSpanEl, iconEl, tooltipEl, dayDiv;
@@ -178,7 +169,7 @@ function renderWeatherData(data) {
 }
 
 // Render forecast data
-function renderForecastData(data) {
+function renderForecastData() {
   //
   var forecastDiv = $("#forecast");
   var spanEl, innerSpanEl, iconEl, tooltipEl, dayDiv;
@@ -201,7 +192,7 @@ function renderForecastData(data) {
     //
     spanEl = $("<span>");
     spanEl.addClass("d-block");
-    spanEl.text(cityWeather.nextFiveDays[i].date);
+    spanEl.text(cityWeather.fiveDaysForecast[i].date);
     spanEl.appendTo(dayDiv);
     //
     // Icon
@@ -209,17 +200,20 @@ function renderForecastData(data) {
     spanEl = $("<span>");
     iconEl = $("<img>");
     iconEl.addClass("text-white");
-    iconEl.attr("src", getWeatherIcon(cityWeather.nextFiveDays[i].weatherIcon));
-    iconEl.attr("alt", cityWeather.nextFiveDays[i].weatherDescription);
+    iconEl.attr(
+      "src",
+      getWeatherIcon(cityWeather.fiveDaysForecast[i].weatherIcon)
+    );
+    iconEl.attr("alt", cityWeather.fiveDaysForecast[i].weatherDescription);
     // iconEl.attr("data-bs-toggle", "tooltip");
     // iconEl.attr("data-bs-placement", "right");
     // iconEl.attr("data-bs-html", "true");
-    // iconEl.attr("title", cityWeather.nextFiveDays[i].weatherDescription);
+    // iconEl.attr("title", cityWeather.fiveDaysForecast[i].weatherDescription);
     iconEl.appendTo(spanEl);
     //
     tooltipEl = $("<span>");
     tooltipEl.addClass("h6 text-capitalize font-italic");
-    tooltipEl.text(cityWeather.nextFiveDays[i].weatherDescription);
+    tooltipEl.text(cityWeather.fiveDaysForecast[i].weatherDescription);
     tooltipEl.appendTo(spanEl);
     //
     spanEl.appendTo(dayDiv);
@@ -231,7 +225,7 @@ function renderForecastData(data) {
     spanEl.text("Temperature: ");
     //
     innerSpanEl = $("<span>");
-    innerSpanEl.text(cityWeather.nextFiveDays[i].temperature + " °F");
+    innerSpanEl.text(cityWeather.fiveDaysForecast[i].temperature + " °F");
     innerSpanEl.appendTo(spanEl);
     //
     spanEl.appendTo(dayDiv);
@@ -243,7 +237,7 @@ function renderForecastData(data) {
     spanEl.text("Wind: ");
     //
     innerSpanEl = $("<span>");
-    innerSpanEl.text(cityWeather.nextFiveDays[i].wind + " MPH");
+    innerSpanEl.text(cityWeather.fiveDaysForecast[i].wind + " MPH");
     innerSpanEl.appendTo(spanEl);
     //
     spanEl.appendTo(dayDiv);
@@ -255,7 +249,7 @@ function renderForecastData(data) {
     spanEl.text("Humidity: ");
     //
     innerSpanEl = $("<span>");
-    innerSpanEl.text(cityWeather.nextFiveDays[i].humidity + " %");
+    innerSpanEl.text(cityWeather.fiveDaysForecast[i].humidity + " %");
     innerSpanEl.appendTo(spanEl);
     //
     spanEl.appendTo(dayDiv);
@@ -267,19 +261,19 @@ function renderForecastData(data) {
     spanEl.text("UV Index: ");
     //
     innerSpanEl = $("<span>");
-    innerSpanEl.text(cityWeather.nextFiveDays[i].uvIndex);
+    innerSpanEl.text(cityWeather.fiveDaysForecast[i].uvIndex);
     //
     // https://www.epa.gov/sunsafety/uv-index-scale-0
     //
-    if (cityWeather.nextFiveDays[i].uvIndex <= 2) {
+    if (cityWeather.fiveDaysForecast[i].uvIndex <= 2) {
       //
       innerSpanEl.addClass("badge bg-success text-white px-3");
       //
-    } else if (cityWeather.nextFiveDays[i].uvIndex <= 7) {
+    } else if (cityWeather.fiveDaysForecast[i].uvIndex <= 7) {
       //
       innerSpanEl.addClass("badge bg-warning text-dark px-3");
       //
-    } else if (cityWeather.nextFiveDays[i].uvIndex >= 8) {
+    } else if (cityWeather.fiveDaysForecast[i].uvIndex >= 8) {
       //
       innerSpanEl.addClass("badge bg-danger text-white px-3");
       //
@@ -295,9 +289,8 @@ function renderForecastData(data) {
 }
 
 // Get latitude/longitude coordinates
-function getCoordinates() {
+function getCoordinates(searchCity) {
   //
-  var searchCity = getSearchByCity();
   var apiURL =
     API_SERVER +
     API_WEATHER_END_POINT +
@@ -324,6 +317,24 @@ function getCoordinates() {
       //
     })
     .then(function (data) {
+      //
+      // Clear the cityWeather object
+      //
+      cityWeather = {
+        date: "",
+        cityName: "",
+        latitude: "",
+        longitude: "",
+        weatherIcon: "",
+        weatherDescription: "",
+        temperature: "",
+        wind: "",
+        humidity: "",
+        uvIndex: "",
+        fiveDaysForecast: [],
+      };
+      //
+      // Store city data
       //
       cityWeather.date = todayText;
       cityWeather.cityName = data.name;
@@ -387,12 +398,18 @@ function getWeatherData() {
         uvIndex: "",
       };
       //
+      // Store weather/forecast data
+      //
       cityWeather.weatherIcon = data.current.weather[0].icon;
       cityWeather.weatherDescription = data.current.weather[0].main;
       cityWeather.temperature = data.current.temp;
       cityWeather.wind = data.current.wind_speed;
       cityWeather.humidity = data.current.humidity;
       cityWeather.uvIndex = data.current.uvi;
+      //
+      // Clear the forecast array
+      //
+      cityWeather.fiveDaysForecast = [];
       //
       for (var i = 0; i < FORECAST_DAYS; i++) {
         //
@@ -407,12 +424,14 @@ function getWeatherData() {
           uvIndex: data.daily[i].uvi,
         };
         //
-        cityWeather.nextFiveDays.push(dailyForecast);
+        cityWeather.fiveDaysForecast.push(dailyForecast);
         //
       }
       //
-      renderWeatherData(data);
-      renderForecastData(data);
+      renderWeatherData();
+      renderForecastData();
+      saveLastSearchCities();
+      renderLastSearchButtons();
       //
     })
     .catch(function (error) {
@@ -425,22 +444,139 @@ function getWeatherData() {
   //
 }
 
-// Initialize dashbaord
-function initializeDashboard() {
+// Set last search cities to localStorage
+function saveLastSearchCities() {
   //
-  cityWeather = {
-    date: "",
-    cityName: "",
-    latitude: "",
-    longitude: "",
-    weatherIcon: "",
-    weatherDescription: "",
-    temperature: "",
-    wind: "",
-    humidity: "",
-    uvIndex: "",
-    nextFiveDays: [],
-  };
+  var index = isLastSearchAlreadyStored();
+  //
+  if (index < 0) {
+    // Insert it
+    lastSearch = {
+      cityName: cityWeather.cityName,
+      latitude: cityWeather.latitude,
+      longitude: cityWeather.longitude,
+    };
+    //
+    lastSearchCities.push(lastSearch);
+    localStorage.setItem("lastSearchCities", JSON.stringify(lastSearchCities));
+    //
+  }
+}
+
+// Get lastSearchCities from localStorage
+function retrieveLastSearchCities() {
+  //
+  var savedSearches = JSON.parse(localStorage.getItem("lastSearchCities"));
+  //
+  if (savedSearches != null) {
+    //
+    lastSearchCities = savedSearches;
+    //
+  }
+  //
+}
+
+// Check if the item is already in the lastSearchCities array
+function isLastSearchAlreadyStored() {
+  //
+  // If found, returns the array index; otherwise, -1
+  //
+  var index = -1;
+  //
+  if (lastSearchCities != null) {
+    //
+    for (var i = 0, l = lastSearchCities.length; i < l; i++) {
+      //
+      if (lastSearchCities[i].cityName === cityWeather.cityName) {
+        index = i;
+        break;
+      }
+      //
+    }
+    //
+  }
+  //
+  return index;
+  //
+}
+
+// Render last search buttons
+function renderLastSearchButtons() {
+  //
+  var buttonEl;
+  //
+  lastSearchCitiesDiv.empty();
+  //
+  for (var i = 0, l = lastSearchCities.length; i < l; i++) {
+    //
+    buttonEl = $("<button>");
+    buttonEl.attr("cityName", lastSearchCities[i].cityName);
+    buttonEl.attr("latitude", lastSearchCities[i].latitude);
+    buttonEl.attr("longitude", lastSearchCities[i].longitude);
+    buttonEl.addClass("btn btn-link");
+    buttonEl.text(lastSearchCities[i].cityName);
+    //
+    buttonEl.appendTo(lastSearchCitiesDiv);
+    //
+  }
+  //
+}
+
+// Get city by means of the search button
+function getCityBySearch() {
+  //
+  var searchString = $("#searchText").val().trim();
+  //
+  if (searchString === "") {
+    //
+    searchString = "Overland Park,KS,US"; // Default
+    //
+  }
+  //
+  return searchString;
+  //
+}
+
+// Get city by means of selection
+function getCityBySelection() {
+  //
+  cityWeather.date = todayText;
+  cityWeather.cityName = $(this).attr("cityName");
+  cityWeather.latitude = $(this).attr("latitude");
+  cityWeather.longitude = $(this).attr("longitude");
+  //
+  $("#searchText").val(cityWeather.cityName);
+  //
+  getWeatherData();
+  //
+}
+
+// Initialize parameters
+function initializeParameters() {
+  //
+  retrieveLastSearchCities();
+  //
+  if (lastSearchCities.length > 0) {
+    //
+    // Get weather data for the first city in the lastSearchCities array
+    //
+    cityWeather.date = todayText;
+    cityWeather.cityName = lastSearchCities[0].cityName;
+    cityWeather.latitude = lastSearchCities[0].latitude;
+    cityWeather.longitude = lastSearchCities[0].longitude;
+    //
+    getWeatherData();
+    //
+  } else {
+    //
+    // Get "local" weather data
+    // TODO: retrieve the actual city
+    //
+    var searchCity = "Overland Park,KS,US";
+    //
+    getCoordinates(searchCity);
+    //
+  }
   //
 }
 
@@ -448,12 +584,17 @@ function initializeDashboard() {
 searchBtn.on("click", function () {
   //
   event.preventDefault();
-  initializeDashboard();
-  getCoordinates();
+  //
+  var searchCity = getCityBySearch();
+  //
+  getCoordinates(searchCity);
+  saveLastSearchCities();
+  renderLastSearchButtons();
   //
 });
 
+lastSearchCitiesDiv.on("click", "button", getCityBySelection);
+
 // Rock & Roll
-initializeDashboard();
-getCoordinates();
+initializeParameters();
 //
